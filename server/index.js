20c606da541796cbb7b118dcb5b6a32543d85497
Anuhya Samudrala <sgn.anuhya@gmail.com>
2024-05-07 01:16:46 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
-
-import express, { json } from "express";
-const app = express();
-
-const port = process.env.PORT || 3000;
-import jwt from "jsonwebtoken";
-import {expressjwt} from "express-jwt";
+import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import { expressjwt } from "express-jwt";
 import connectDB from "./config/database.js";
 import BudgetRoute from "./routes/BudgetRoute.js";
 import UserRoute from "./routes/UserRoute.js";
@@ -13,29 +9,31 @@ import expenseRoute from "./routes/ExpenseRoute.js";
 import incomeRoute from "./routes/IncomeRoute.js";
 import BudgetsRoute from "./routes/BudgetsRoute.js";
 
-const baseUrl = process.env.BASE_URL;
+const app = express();
+const port = process.env.PORT || 3000;
 
-
+// Enable CORS for all routes
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
-    next();
-})
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
-});
 
 app.get("/", (req, res) => {
     console.log("Welcome to the server");
+    res.send("Welcome to the server");
 });
 
+// Connect to the database
 connectDB();
+
+// Mount the routes
 app.use("/budget", BudgetRoute);
 app.use("/user", UserRoute);
 app.use("/expenses", expenseRoute);
-app.use("/income", incomeRoute)
-app.use("/budgets", BudgetsRoute)
+app.use("/income", incomeRoute);
+app.use("/budgets", BudgetsRoute);
 
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
